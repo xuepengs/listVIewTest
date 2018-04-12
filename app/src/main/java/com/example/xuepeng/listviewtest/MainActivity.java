d230ea7fd8a4af1929e51ce3c1d4a66413a9eeff
xuepeng <helloxuepeng@gmail.com>
2018-04-12 21:22:17 +0800

    优化ListView点击事件

--- app/src/main/java/com/example/xuepeng/listviewtest/MainActivity.java
+++ app/src/main/java/com/example/xuepeng/listviewtest/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.xuepeng.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
         listView=findViewById(R.id.listview);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Fruit fruit =fruitList.get(i);
+                Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
